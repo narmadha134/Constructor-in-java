30fb85cbf5c9dc0df9b60b51e57f41dc229f78a5
narmadha134 <krnarmadhaofficial03@gmail.com>
2026-05-01 14:23:53 +0530

    Constructors in java
    Signed-off-by: narmadha134 <krnarmadhaofficial03@gmail.com>

---
+++ constructor.java
@@ -0,0 +1,27 @@
+class cons {
+    public static void main(String[] args) {
+        constructors o = new constructors("Narmadha");
+        constructors o2 = new constructors(o);
+        constructors o3 = new constructors();
+
+        System.out.println(o.name);
+        System.out.println(o2.name2);
+    }
+}
+
+class constructors {
+    String name;
+    String name2;
+
+    constructors() {
+        System.out.println("Hi");
+    }
+
+    constructors(String mn) {
+        name = mn;
+    }
+
+    constructors(constructors j) {
+        name2 = j.name;
+    }
+}
